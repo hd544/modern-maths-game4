88e2d274bbf2121ce71e765851a0968b2f2a27f1
Hollie Davis <146210945+hd544@users.noreply.github.com>
2024-07-20 12:03:27 +0000

    commit

--- Game/Levels/PALsessions/L01_session _1.lean
+++ Game/Levels/PALsessions/L01_session _1.lean
@@ -10,44 +10,20 @@ Let f: ℤ → ℤ be the function f(x):= 2x
 We'll show that the function f is injective.
 "
 
-/-- `assume` is used to show what we assume to be true
--/
-TacticDoc «assume»
-
-NewTactic «assume»
-
-/-- `given` is used to show hypotheses that we already know, rewritten in a different way
--/
-TacticDoc «given»
-
-NewTactic «given»
-
-
 /-- `linarith` is used to prove many linear equations and inequalities
 -/
+
 TacticDoc «linarith»
 
 NewTactic «linarith»
 
-/-- `show` is used to state what is being proved
--/
-TacticDoc «show»
-
-NewTactic «show»
-
-
-namespace MyGroup
-
-open Group
-
-variable {G : Type} [Group G]
-
 
 /-- Let f: ℤ → ℤ be the function f(x):=2x. Prove that f is injective. -/
-Statement eq_one_of_self_mul_eq (b : G) (h : ∀ (a : G), b * a = a) : b = 1 := by
-
+def f (x : ℕ) : ℕ := 2 * x
+Statement injective f : by
+intros x₁ x₂ h
+have h_eq : 2 * x₁ = 2 * x₂ := h
+linarith
 Conclusion "
 Well done!
 "
-
-end MyGroup
